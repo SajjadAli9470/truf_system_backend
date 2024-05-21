3bdb1da36ed17d980ca26ebb612985a3d40398f8
Sajjad Ali <78296469+SajjadAli9470@users.noreply.github.com>
2024-05-22 03:53:48 +0500

    port changed

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+
+
+
 // Import necessary modules
 const express = require('express');
 const mysql = require('mysql');
@@ -486,7 +490,7 @@ app.post('/fetch_bookings_by_ground_name', (req, res) => {
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
