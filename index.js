ff934b886a5705eec85965668332e9d69176a23c
Sajjad Ali <78296469+SajjadAli9470@users.noreply.github.com>
2024-05-22 06:17:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -499,11 +499,11 @@ app.use(express.json());
 // console.log("Connecting the listner...");
 // // Start the server
 
-// // app.listen(
+app.listen(
 
-// // );
+);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
